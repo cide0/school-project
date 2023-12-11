abb29fa6bbed42544eeded0ba6497d78b1b367af
lschneidenbach <lschneidenbach@systhemis.de>
2023-12-11 10:59:37 +0100

    #17 Hompage base

--- code/public/app/src/app/app.module.ts
+++ code/public/app/src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {DashboardComponent} from "./homepage/dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomepageComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
