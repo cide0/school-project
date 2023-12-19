66f69a5ee7a5a5a64e604c31a3d7749799d6c9a6
waterchickn <milo.ivaiva@gmail.com>
2023-12-19 12:41:41 +0100

    #22

---
+++ code/public/app/src/app/livecampage/livecampage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livecampage',
+  templateUrl: './livecampage.component.html',
+  styleUrls: ['./livecampage.component.css']
+})
+export class LivecampageComponent {
+
+}
